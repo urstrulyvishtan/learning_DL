d4ef0f95aa709af1aec6b9068fe40788666b1284
Sibi Vishtan <64768616+urstrulyvishtan@users.noreply.github.com>
2024-01-07 12:55:37 -0500

    group anagrams

---
+++ LeetCode/Medium_Leetcode/49.Group_anagrams/Ganagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def anagrams_group(self, strs: list[str]) -> list[list[str]]:
+        values = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            values[tuple(sorted(s))].append(s)
+        return values.values()
