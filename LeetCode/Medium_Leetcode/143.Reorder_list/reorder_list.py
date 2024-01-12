2d890b1195867d5ccd69060adb08b88870cfcc44
Sibi Vishtan <64768616+urstrulyvishtan@users.noreply.github.com>
2024-01-11 23:23:21 -0500

    99% order

--- LeetCode/Medium_Leetcode/143.Reorder_list/reorder_list.py
+++ LeetCode/Medium_Leetcode/143.Reorder_list/reorder_list.py
@@ -5,20 +5,21 @@ class ListNode:
 
 class Solution:
     def reorderList(self, head)-> None:
-        if not head:
-            return None
-        # Find the middle of the list
-        slow = fast = head
+        slow, fast = head, head.next
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        # Reverse the second half of the list
-        prev, curr = None, slow
-        while curr:
-            curr.next, prev, curr = prev, curr, curr.next
-        # Merge two lists
+            slow, fast = slow.next, fast.next.next
+        
+        second = slow.next
+        prev =slow.next= None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
         first, second = head, prev
-        while second.next:
-            first.next, first = second, first.next
-            second.next, second = first, second.next
-        return head
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
