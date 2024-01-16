3a43d2744f7240deb85a79f64d930fda9b31f517
Sibi Vishtan <64768616+urstrulyvishtan@users.noreply.github.com>
2024-01-15 19:41:06 -0500

    test pass

--- LeetCode/Medium_Leetcode/213.House_robber_2/house_robber_in_circle.py
+++ LeetCode/Medium_Leetcode/213.House_robber_2/house_robber_in_circle.py
@@ -1,5 +1,7 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
+        if len(nums) == 0:
+            return 0
         
         return max(nums[0], self.rob_helper(nums[:-1]), self.rob_helper(nums[1:]))
     def rob_helper(self, nums: list[int])-> int:
