79f847b0cb39b91132a7a30877f684c7a47b0ffc
Sibi Vishtan <64768616+urstrulyvishtan@users.noreply.github.com>
2024-01-15 19:40:44 -0500

    house robber 2 test error with null

---
+++ LeetCode/Medium_Leetcode/213.House_robber_2/house_robber_in_circle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        
+        return max(nums[0], self.rob_helper(nums[:-1]), self.rob_helper(nums[1:]))
+    def rob_helper(self, nums: list[int])-> int:
+        rob1, rob2 = 0,0 
+        for i in nums:
+            temp = max(i + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+    
