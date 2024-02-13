990b1aff09b7237b1bdeb875632b678d8a85f049
Sibi Vishtan <sibivishtantt@gmail.com>
2024-02-13 18:44:15 -0500

    done with gradient

--- neetcode.io_ML/gradient/gradient.py
+++ neetcode.io_ML/gradient/gradient.py
@@ -1,3 +1,9 @@
+# Given the number of iterations to perform gradient descent, the learning rate, and an initial guess, return the value of x that globally minimizes this function.
+
+# Round your final result to 5 decimal places using Python's round() function.
 class Solution:
     def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        
+        x = init
+        for _ in range(iterations):
+            x -= learning_rate * (2 * x)
+        return round(x, 5)
