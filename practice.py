e1b91ece3991df4bdeabc015e561e51500e7cd1f
urstrulyvishtan <thirukonda.s@northeastern.edu>
2024-10-12 14:58:17 -0400

    product of array itself

--- practice.py
+++ practice.py
@@ -1,22 +1,14 @@
 class Solution:
-    def minRemoveToMakeValid(self, s: str) -> str:
-        res = []
-        cnt = 0
-        for c in s:
-            if c == "(":
-                cnt +=1
-                res.append(c)
-            elif c == ")" and cnt>0:
-                cnt-=1
-                res.append(c)
-            elif c != ")":
-                res.append(c)
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
 
-        filtered = []
-        for c in res[::-1]:
-            if c == "(" and cnt>0:
-                cnt-=1
-            else:
-                filtered.append(c)
-        
-        return "".join(filtered[::-1])
+        prefix = 1
+        for i in range (len(nums)):
+            res[i] *= prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
