41ddf017fadb1dc0057f3de33e29c0dc0e7fe703
urstrulyvishtan <thirukonda.s@northeastern.edu>
2024-10-13 17:57:23 -0400

    integer to words

--- practice.py
+++ practice.py
@@ -1,13 +1,36 @@
 class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixSum = {0:1}
-        currSum = 0
-        res = 0
-        for i in nums:
-            currSum += i
-            diff = currSum - k
+    def numberToWords(self, num: int) -> str:
+        if num == 0:
+            return "Zero"
+        
+        bigString = ["Thousand", "Million", "Billion"]
+        result = self.numberToWordsHelper(num % 1000)
+        num //= 1000
+        
+        for i in range(len(bigString)):
+            if num > 0 and num % 1000 > 0:
+                result = self.numberToWordsHelper(num % 1000) + bigString[i] + " " + result
+            num //= 1000
+        
+        return result.strip()
 
-            res += prefixSum.get(diff, 0)
-            prefixSum[currSum] = 1+prefixSum.get(currSum, 0)
-
-        return res
+    def numberToWordsHelper(self, num: int) -> str:
+        digitString = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        teenString = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        tenString = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        
+        result = ""
+        if num > 99:
+            result += digitString[num // 100] + " Hundred "
+        
+        num %= 100
+        if num < 20 and num > 9:
+            result += teenString[num - 10] + " "
+        else:
+            if num >= 20:
+                result += tenString[num // 10] + " "
+            num %= 10
+            if num > 0:
+                result += digitString[num] + " "
+        
+        return result
