bb5cc4e3884d32c11389ccf6de86f924a916b0a5
urstrulyvishtan <thirukonda.s@northeastern.edu>
2024-10-13 20:18:02 -0400

    binary tree max path sum meta

--- practice.py
+++ practice.py
@@ -1,19 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def addStrings(self, num1: str, num2: str) -> str:
-        i, j = len(num1) - 1, len(num2) - 1  # Pointers to the end of each string
-        carry = 0  # Initialize carry
-        result = []  # To store the final result in reverse order
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        global_max = float('-inf')
 
-        # Add digits from both strings starting from the end
-        while i >= 0 or j >= 0 or carry:
-            digit1 = ord(num1[i]) - ord('0') if i >= 0 else 0  # Get digit from num1 or use 0 if i is out of bounds
-            digit2 = ord(num2[j]) -ord('0') if j >= 0 else 0  # Get digit from num2 or use 0 if j is out of bounds
+        def dfs(node):
+            nonlocal global_max
+            if not node:
+                return 0
             
-            total = digit1 + digit2 + carry  # Add digits and carry
-            carry = total // 10  # Update carry
-            result.append(chr(total%10+ord('0')))  # Append the digit to result
-            
-            i -= 1  # Move left in num1
-            j -= 1  # Move left in num2
+            left_max = max(dfs(node.left), 0)
+            right_max = max(dfs(node.right), 0)
 
-        return ''.join(result[::-1])  # Reverse the result list and join to form the final string
+            current_max = node.val+left_max+right_max
+            global_max = max(global_max, current_max)
+            return node.val + max(left_max, right_max)
+
+        dfs(root)
+        return global_max
+
+
