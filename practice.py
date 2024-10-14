5ded420b3f58b6e67394c26e0c27e260df5a1dd4
urstrulyvishtan <thirukonda.s@northeastern.edu>
2024-10-14 17:45:53 -0400

    sliding window find anagrams meta

--- practice.py
+++ practice.py
@@ -4,10 +4,16 @@ class Solution:
             return[]
         
         result = []
-        sorted_p = sorted(p)
+        p_count = [0]*26
+        s_count = [0]*26
 
-        for i in range(len(s) - len(p) + 1):
-            substring = s[i:i+len(p)]
-            if sorted(substring) == sorted_p:
-                result.append(i)
+        for char in p:
+            p_count[ord(char)-ord('a')] += 1
+
+        for i in range(len(s)):
+            s_count[ord(s[i]) - ord('a')]+=1
+            if i>=len(p):
+                s_count[ord(s[i-len(p)]) - ord('a')]-=1
+            if s_count == p_count:
+                result.append(i-len(p)+1)
         return result
