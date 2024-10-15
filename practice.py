81f4bc79ba7c15cb53068f84e0686911c652b58d
urstrulyvishtan <thirukonda.s@northeastern.edu>
2024-10-15 15:28:05 -0400

    palindrome meta O(1) space

--- practice.py
+++ practice.py
@@ -1,7 +1,17 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        cleaned = ''.join(char.lower() for char in s if char.isalnum())
-        return cleaned == cleaned[::-1]
+    def isPalindrome(self, s: str) -> bool: # race car
+        left, right = 0, len(s)-1 # 0 7
+        while left<right:
+            while left<right and not s[left].isalnum(): 
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
 
 # convert string to lower case
 # remove all non alpha numeric chars
@@ -11,3 +21,11 @@ class Solution:
 # time complexity : o(n)
 # space complexity : O(n)
 
+# init left, right
+# while left<right skip any non alphanumeric char for both pointers
+# compare the char at left and right
+# if they match continue inward otherwise return False
+# if all valid char match return true
+
+# time complexity O(n)
+# space complexity O(1)
