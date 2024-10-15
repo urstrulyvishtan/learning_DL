31bdb2e40d509b99f11597be9185b5b15366afa1
urstrulyvishtan <thirukonda.s@northeastern.edu>
2024-10-14 21:27:37 -0400

    design add search words data struct meta

--- practice.py
+++ practice.py
@@ -1,19 +1,51 @@
-class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)> len(s):
-            return[]
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.is_end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(index, node):
+            if index == len(word):
+                return node.is_end
+            char = word[index]
+            if char == '.':
+                for child in node.children:
+                    if dfs(index+1, node.children[child]):
+                        return True
+                return False
+            else:
+                if char in node.children:
+                    return dfs(index+1, node.children[char])
+                else:
+                    return False
+        return dfs(0, self.root)
         
-        result = []
-        p_count = [0]*26
-        s_count = [0]*26
 
-        for char in p:
-            p_count[ord(char)-ord('a')] += 1
 
-        for i in range(len(s)):
-            s_count[ord(s[i]) - ord('a')]+=1
-            if i>=len(p):
-                s_count[ord(s[i-len(p)]) - ord('a')]-=1
-            if s_count == p_count:
-                result.append(i-len(p)+1)
-        return result
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+# init dict class with root node empty trie
+# traverse the trie for each char in the word, create new node if it doesn't already exist
+# search word : if a char is . : check all possible branches
+# otherwise continue the trie traversal
+
+# add bad, b-> creates new node, a, d, dad, pad, mad
+# search pad, p doesn't exist so return false..
+# search bad, b->a->d word marked return true
+# search .ad, .-> all the child nodes, b, d, m. a->d matches for b. return True
+# search b.. -> next, try all children of a , and d is found so return True.
